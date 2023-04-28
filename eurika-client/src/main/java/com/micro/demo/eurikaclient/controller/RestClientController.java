a3bf7607f90935bd99376d0810de818c053dbc34
Nami <xarofive@gmail.com>
2023-04-28 15:37:11 +0300

    added spring cloud config server based on github

--- eurika-client/src/main/java/com/micro/demo/eurikaclient/controller/RestClientController.java
+++ eurika-client/src/main/java/com/micro/demo/eurikaclient/controller/RestClientController.java
@@ -1,5 +1,6 @@
 package com.micro.demo.eurikaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class RestClientController {
-    @GetMapping("/test")
-    public String test() {
-        return "zzzzzzzzzzzzzzzzz";
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
+    @GetMapping("/id")
+    public String getId() {
+        return id;
     }
 }
