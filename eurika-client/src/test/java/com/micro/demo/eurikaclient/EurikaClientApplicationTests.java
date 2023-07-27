5f4383ef454aa94b4840eb49937ec00e4d31a680
Nami <xarofive@gmail.com>
2023-07-27 08:54:35 +0300

    update properties

--- eurika-client/src/test/java/com/micro/demo/eurikaclient/EurikaClientApplicationTests.java
+++ eurika-client/src/test/java/com/micro/demo/eurikaclient/EurikaClientApplicationTests.java
@@ -1,13 +1,13 @@
-package com.micro.demo.eurikaclient;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EurikaClientApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.micro.demo.eurikaclient;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EurikaClientApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
